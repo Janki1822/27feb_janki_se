a00fcbbc1da20a52703a1299aa48d30e7b090bca
Janki Kaushal patel <128150518+Janki1822@users.noreply.github.com>
2023-04-16 20:41:10 +0530

    Add files via upload

---
+++ taskes.c/task.c/2d-array - task.c
@@ -0,0 +1,32 @@
+//WAP of 2d-Array taskes 
+#include<stdio.h>
+int main()
+{
+	/*
+	complete the task
+	1 2 3 
+	4 5 6
+	7 8 9
+	*/
+	int a[20][20];
+	int i, j;
+	for (i =0; i < 3; i++)
+	{
+		for (j = 0; j < 3; j++)
+		{
+			printf("a[%d][%d]", i, j);
+			scanf("%d", &a[i][j]);
+		}
+		printf("\n");
+	}
+	
+	for(i = 0; i < 3; i++)
+	{
+		for(j = 0; j < 3; j++)
+		{
+			printf("%d ", a[i][j]);
+		}
+		printf("\n");
+	}
+	return 0;
+}
