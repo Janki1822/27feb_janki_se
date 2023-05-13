8dcc971076cb52133b1815d026553ce7d55b527f
Janki Kaushal patel <128150518+Janki1822@users.noreply.github.com>
2023-05-13 22:20:29 +0530

    Add files via upload

---
+++ work/Bank account.cpp
@@ -0,0 +1,8 @@
+//Define a class to represent a bank account.
+#include<iostream>
+using namespace std;
+class Bank
+{
+	public:
+		
+}
---
+++ work/Modual-4 SE.OOP concept/Hello world.cpp
@@ -0,0 +1,6 @@
+#include<iostream>// using iostream - input output stream header file.
+int main()
+{
+	std::cout<<"Hello World"; //Print the (Hello World) take the users input.
+	return 0;
+}
