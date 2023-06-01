94a8dc93eb1238b95e1e9c9dce17f133fd6be635
Janki Kaushal patel <128150518+Janki1822@users.noreply.github.com>
2023-06-01 23:29:29 +0530

    Update 17 - Swap-Tamplate.cpp

--- work/Modul-4 SE.OOPS/17 - Swap-Tamplate.cpp
+++ work/Modul-4 SE.OOPS/17 - Swap-Tamplate.cpp
@@ -3,7 +3,7 @@
 #include<iostream>
 using namespace std;
 template <class T> //Use Function Template 
-int sn(T&a,T&b) // take variable for output
+int sn(T&a,T&b)    // take variable for output
 {
 	T t; // Take the T class name for swap the value.
 	t = a;
@@ -13,9 +13,9 @@ int sn(T&a,T&b) // take variable for output
 }
 int main()
 {
-	int j,k; // Take two variable for swap the number.
+	int j,k;       // Take two variable for swap the number.
 	j = 30, k= 10; // give the value of two variable .
-	sn (j,k); // Call the swap number
+	sn (j,k);      // Call the swap number
 	cout<<endl<<j; // get output to print the value
 	cout<<endl<<k;
 	return 0;
